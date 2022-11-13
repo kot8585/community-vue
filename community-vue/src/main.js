cfed3aaf0da07cf379df5d1dad00340c049cbd14
kot8585 <ansgywjd321@gmail.com>
2022-11-13 18:53:31 +0900

    홈 화면 운곽잡기
    디테일한 부분 수정 필요

--- community-vue/src/main.js
+++ community-vue/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './input.css'
 import App from './App.vue'
 import '/dist/output.css'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
